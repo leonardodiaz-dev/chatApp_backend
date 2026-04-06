6cc749668b2355ab8daaa7d645bf811e9c2e2832
Leonardo Diaz <diazleonardo237@gmail.com>
2026-04-06 17:28:46 -0500

    feat:agrega nueva ruta

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ Route::get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/conversations/contact',[ConversationController::class,'getContacts'])->middleware('auth:sanctum');
 Route::get('/conversations',[ConversationController::class,'getAllConversations'])->middleware('auth:sanctum');
 Route::post('/conversations',[ConversationController::class,'store'])->middleware('auth:sanctum');
 
